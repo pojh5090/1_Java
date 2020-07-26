2c8ad78ff6aa0488e03d28b197c5e9db0b6617ba
pojh5090 <pojh4@naver.com>
2020-07-26 22:54:19 +0900

    0726

---
+++ 1_Java_workspace/09_polymorphism/src/com/kh/practice/chap02_abstractNInterface/model/vo/NotePen.java
@@ -0,0 +1,6 @@
+package com.kh.practice.chap02_abstractNInterface.model.vo;
+
+public interface NotePen {
+	public static final boolean PEN_BUTTON = true;
+	public abstract boolean bluetoothPen();
+}
